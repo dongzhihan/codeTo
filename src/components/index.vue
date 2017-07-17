80100533b29b60b5f337739e71b6f50bdb62500e
dongzhihan <529211933@qq.com>
2017-07-17 15:57:04 +0800

    push  blockly

--- src/components/index.vue
+++ src/components/index.vue
@@ -9,14 +9,86 @@
       </div>
     </div>-->
     <div id="blocklyDiv" style="height: 480px; width: 600px;"></div>
-      <xml id="toolbox" style="display: none">
-    <block type="controls_if"></block>
-    <block type="logic_compare"></block>
-    <block type="controls_repeat_ext"></block>
-    <block type="math_number"></block>
-    <block type="math_arithmetic"></block>
-    <block type="text"></block>
-    <block type="text_print"></block>
+   
+    <div v-on:click="codeTo()" style="height: 100px; width: 100px;">code to </div>
+    <div>{{codeJs}}</div>
+   <xml id="toolbox" style="display: none">
+    <category name="Logic">
+      <block type="controls_if"></block>
+      <block type="logic_compare"></block>
+      <block type="logic_operation"></block>
+      <block type="logic_negate"></block>
+      <block type="logic_boolean"></block>
+    </category>
+    <category name="Loops">
+      <block type="controls_repeat_ext">
+        <value name="TIMES">
+          <block type="math_number">
+            <field name="NUM">10</field>
+          </block>
+        </value>
+      </block>
+      <block type="controls_whileUntil"></block>
+    </category>
+    <category name="Math">
+      <block type="math_number"></block>
+      <block type="math_arithmetic"></block>
+      <block type="math_single"></block>
+    </category>
+    <category name="Text">
+      <block type="text"></block>
+      <block type="string_length"></block>
+      <block type="text_length"></block>
+      <block type="text_print"></block>
+    </category>
+  </xml>
+  <xml id="startBlocks" style="display: none">
+    <block type="controls_if" inline="false" x="20" y="20">
+      <mutation else="1"></mutation>
+      <value name="IF0">
+        <block type="logic_compare" inline="true">
+          <field name="OP">EQ</field>
+          <value name="A">
+            <block type="math_arithmetic" inline="true">
+              <field name="OP">MULTIPLY</field>
+              <value name="A">
+                <block type="math_number">
+                  <field name="NUM">6</field>
+                </block>
+              </value>
+              <value name="B">
+                <block type="math_number">
+                  <field name="NUM">7</field>
+                </block>
+              </value>
+            </block>
+          </value>
+          <value name="B">
+            <block type="math_number">
+              <field name="NUM">42</field>
+            </block>
+          </value>
+        </block>
+      </value>
+      <statement name="DO0">
+        <block type="text_print" inline="false">
+          <value name="TEXT">
+            <block type="text">
+              <field name="TEXT">Don't panic</field>
+            </block>
+          </value>
+        </block>
+      </statement>
+      <statement name="ELSE">
+        <block type="text_print" inline="false">
+          <value name="TEXT">
+            <block type="text">
+              <field name="TEXT">Panic</field>
+            </block>
+          </value>
+        </block>
+      </statement>
+    </block>
   </xml>
   </div>
 
@@ -47,48 +119,16 @@
   export default {
     data() {
       return {
-        defaultValue: '',
-        imgList: [
-          'http://placeholder.qiniudn.com/800x300/FF3B3B/ffffff',
-          'http://placeholder.qiniudn.com/800x300/FFEF7D/ffffff',
-          'http://placeholder.qiniudn.com/800x300/8AEEB1/ffffff'
-        ],
-        bedTypes: [
-
-          {
-            src: 'https://oixyh3u6e.qnssl.com/livingearth/livingearth.png',
-            id: 5,
-            title: '大床房',
-            desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-          },
-          {
-            src: 'https://oixyh3u6e.qnssl.com/livingearth/livingearth.png',
-            id: 6,
-            title: 'no2'
-          },
-          {
-            src: 'https://oixyh3u6e.qnssl.com/livingearth/livingearth.png',
-            id: 7,
-            title: 'no3'
-          },
-          {
-            src: 'https://oixyh3u6e.qnssl.com/livingearth/livingearth.png',
-            id: 8,
-            title: 'no4'
-          },
-          {
-            src: 'https://oixyh3u6e.qnssl.com/livingearth/livingearth.png',
-            id: 9,
-            title: 'no5'
-          }
-        ]
+        workspace: Object,
+        codeJs: ''
       };
     },
     mounted() {
-      var workspace = Blockly.inject('blocklyDiv', {
+      this.workspace = Blockly.inject('blocklyDiv', {
         toolbox: document.getElementById('toolbox')
       });
-      console.dir(workspace);
+ 
+      console.dir(this.workspace);
     },
     components: {
       Selector,
@@ -99,8 +139,14 @@
       Panel
     },
     methods: {
+      codeTo() {
+        console.log(this.workspace);
+       Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+      var code = Blockly.JavaScript.workspaceToCode(this.workspace);
+      alert(code);
+        
+      },
       goTo(item) {
-
         //  this.$http.post(api.xxx, data, api.config).then((data) => {
         // if (data.data.Errcode === 0) {
         this.$router.push({
--- src/main.js
+++ src/main.js
@@ -11,8 +11,10 @@ import FastClick from 'fastclick';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import router from './router/index';
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
 require('babel-polyfill');
+Vue.use(ElementUI);
 
 Vue.use(VueRouter);
 
